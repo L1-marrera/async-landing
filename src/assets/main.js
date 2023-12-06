ca94dd2149be99a5bd4746ca9901039cf90e4b32
Luis De La Rosa <luismarrera10@gmail.com>
2023-12-06 14:49:11 -0400

    Anexando archivo JS a HTML Corrigiendo variable content

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,6 +1,6 @@
 const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCw05fUBPwmpu-ehXFMqfdMw&part=snippet%2Cid&order=date&maxResults=9';
 
-const content = null | document.getElementById('content');
+const content = null || document.getElementById('content');
 
 const options = {
     method: 'GET',
@@ -20,22 +20,24 @@ async function fetchData(urlApi) {
     try {
         const videos = await fetchData(API)
         let view = `
-        ${videos.items.map(video => ` 
-            <div class="group relative">
-                <div
-                    class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-                    <img src="${video.snippet.thumbnail.high.url}" alt="${video.snippet.description}" class="w-full">
+            ${videos.items.map(video => ` 
+                <div class="group relative">
+                    <div
+                        class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+                        <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+                    </div>
+                    <div class="mt-4 flex justify-between">
+                        <h3 class="text-sm text-gray-700">
+                            <span aria-hidden="true" class="absolute inset-0"></span>
+                            ${video.snippet.title}
+                        </h3>
+                    </div>
                 </div>
-                <div class="mt-4 flex justify-between">
-                    <h3 class="text-sm text-gray-700">
-                        <span aria-hidden="true" class="absolute inset-0"></span>
-                        ${video.snippet.title}
-                    </h3>
-                </div>
-            </div>
-        `).slice(0, 4).joint(' ')}
-        `
-    } catch {
+            `).slice(0, 4).join(' ')}
+        `;
+        content.innerHTML = view;
 
+    } catch (error) {
+        console.log(error);
     }
 })();
